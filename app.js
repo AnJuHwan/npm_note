b05eee2dc947363d4074f9f55ae52c47fbc0aab0
JuHwan <wnghks0531@gmail.com>
2021-06-04 09:24:35 +0900

    note

---
+++ app.js
@@ -0,0 +1,24 @@
+/* 
+npm init 
+package name : 패키지 이름(test)
+version : 버젼 (그냥 Enter 누르면 기본값)
+description : 설명
+entry point : 파일의 시작점(app.js)
+test command : 테스트 명령어(넘어가도 됨)
+git repository : git 주소 
+keywords : npm배포했을 때 keyword 검색명
+author : 제작사
+license : ISC (그냥엔터 기본값)
+
+***package.json 수정가능***
+
+npm outdated : 업데이트가 필요한 버전을 확인 가능
+npm uninstall (libraryName) : 라이브러리 삭제
+npm view : 업데이트된 정보를 확인
+npm update (libraryName) : 라이브러리 업데이트
+
+npm i nodemon --save-dev : 개발할 때만 필요한 것은 구분해서 저장
+*/
+
+console.log('hello');
+console.log('안녕!');
